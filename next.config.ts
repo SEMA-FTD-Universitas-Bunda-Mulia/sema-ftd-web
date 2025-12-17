7161f91228ddfab8e87191265b7804c827e06f02
Christopher Haris <christoharis2005@gmail.com>
2025-12-17 11:52:59 +0700

    31: posthog test 1

--- next.config.ts
+++ next.config.ts
@@ -13,19 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
 };
 
 export default withPayload(nextConfig);
--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -1,23 +1,22 @@
-"use client"
+// app/providers.tsx
+"use client";
 
-import posthog from "posthog-js"
-import { PostHogProvider as PHProvider } from "posthog-js/react"
-import { useEffect } from "react"
+// import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+// import { usePostHog } from "posthog-js/react";
 
-interface PostHogProviderProps {
-  children: React.ReactNode
-}
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
 
-export function PostHogProvider({ children }: PostHogProviderProps) {
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      ui_host: "https://us.posthog.com",
-      defaults: '2025-05-24',
-      capture_exceptions: true,
-      debug: process.env.NODE_ENV === "development",
-    })
-  }, [])
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      defaults: "2025-11-30",
+    });
+  }, []);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>
+  return <PHProvider client={posthog}>{children}</PHProvider>;
 }
